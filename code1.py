a04e813dc5f1534dad89c8993eba8eb648597387
Ehsan <ehsan.doroudian@gmail.com>
2024-01-13 10:34:05 +0330

    string_iterable.py

--- code1.py
+++ code1.py
@@ -1,45 +1,30 @@
 def dict_romance_intiger(roman_letter):
-    dict_nums = {
-        "I" : 1,
-        "X" : 10,
-        "C" : 100,
-        "M" : 1000,
-        "V" : 5,
-        "L" : 50,
-        "D" : 500,
-    }
     
+    dict_nums = {"I" : 1, "X" : 10, "C" : 100, "M" : 1000, "V" : 5,  "L" : 50, "D" : 500, }
     return dict_nums[roman_letter]
     
-def process(roman_num : str) -> int:
+def process(roman_letter : str) -> int:
     int_number = 0
-    roman_list = list()
-    for j in roman_num:
-        roman_list.append(j)
-
     i = 0
-    n = len(roman_list)
+    n = len(roman_letter)
     
     while True:
         try:
-            num1, num2 = int(dict_romance_intiger(roman_list[i])), int(dict_romance_intiger(roman_list[i+1]))
-            print(num1, num2)     
-            if num1 > num2 or num1 == num2:
-                int_number += num1
-                i += 1
+            num1, num2 = int(dict_romance_intiger(roman_letter[i])), int(dict_romance_intiger(roman_letter[i+1]))
             
-            else:
-                num = num2 - num1
-                int_number += num
-                i += 2
-        
         except IndexError:
-            if (int(dict_romance_intiger(roman_list[i]))) > (int(dict_romance_intiger(roman_list[i-1]))):
-                pass
-            else:
-                int_number += (int(dict_romance_intiger(roman_list[i])))
-            break
-           
+            if (int(dict_romance_intiger(roman_letter[i]))) < (int(dict_romance_intiger(roman_letter[i-1]))):
+                int_number += (int(dict_romance_intiger(roman_letter[i])))
+                break
+        
+        if num1 > num2 or num1 == num2:
+            int_number += num1
+            i += 1
+
+        else:
+            num = num2 - num1
+            int_number += num
+            i += 2
 
         if i >= n:
             break
